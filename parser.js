29c955a79e18990f3f92da0404a9923e14508125
roma-k <intere07@yandex.ru>
2026-06-08 01:35:53 +0300

    Парсинг метатегов и головы

--- parser.js
+++ parser.js
@@ -1,8 +1,30 @@
 // @todo: напишите здесь код парсера
 
+
 function parsePage() {
+    const lang = document.documentElement.lang || '';
+    const [title, trash] = document.title.trim().split('—'),
+    keywords = document.querySelector('meta[name="keywords"]')?.content.split(',').map(item => item.trim()) || [],
+    description = document.querySelector('meta[name="description"]')?.content || '';
+
+
+    function ogTag(proprty) {
+           return document.querySelector(`meta[property="${proprty}"]`)?.content || '';
+    }
+
+
     return {
-        meta: {},
+        meta: {
+            lang: document.documentElement.lang || 'NO',
+            title: title.trim(),
+            keywords: keywords,
+            description: description.trim(),
+            openGraph: {
+                title: ogTag('og:title'),
+                image: ogTag('og:image'),
+                type: ogTag('og:type'),
+            }
+        },
         product: {},
         suggested: [],
         reviews: []
